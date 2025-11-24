473800fd47bcf3375cea2aebffe5ebb2663e05b1
Reis <artemr0307@gmail.com>
2025-11-24 15:38:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	//делает , чтобы код видел ру язык
+	setlocale(LC_ALL, "RU");
+	//выводит текст , и делает отступ в новую строку -> \n
+
+	std::cout << "All is okey\n";
+	std::cout << "прив";
+
+	return 0;
+}
+
+/*
+	многострочный комент
+	*/
